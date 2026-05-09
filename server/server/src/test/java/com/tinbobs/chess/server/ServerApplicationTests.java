a1df19a5aabf96a74aa037897977b851ce2320fe
Will Rutland <wrutland07@gmail.com>
2026-05-09 22:22:34 +0100

    server init

---
+++ server/server/src/test/java/com/tinbobs/chess/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinbobs.chess.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
