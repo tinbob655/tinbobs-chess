e01736c9c52782d1e29bebbe6b12cabd77743f7c
Will Rutland <wrutland07@gmail.com>
2026-05-11 15:46:19 +0100

    added margins to page header

--- client/src/components/multiPageComponents/pageHeader.tsx
+++ client/src/components/multiPageComponents/pageHeader.tsx
@@ -8,12 +8,14 @@ interface params {
 
 export default function PageHeader({title, subtitle}:params):React.ReactElement {
 
+    const marginTopBottom:string = '30px';
+
     return (
         <React.Fragment>
-            <h1 className="alignLeft" style={{marginLeft: '10%'}}>
+            <h1 className="alignLeft noVerticalSpacing" style={{marginLeft: '10%', marginTop: marginTopBottom}}>
                 {title}
             </h1>
-            <p className="alignLeft" style={{marginLeft: '12.5%'}}>
+            <p className="alignLeft noVerticalSpacing" style={{marginLeft: '12.5%', marginBottom: marginTopBottom}}>
                 {subtitle}
             </p>
             <div className="dividerLine"></div>
