d9432d9aa2e402a042c9dfe791c5012b3a5029bf
Will Rutland <wrutland07@gmail.com>
2026-05-11 20:46:08 +0100

    cached moves to save computation

--- server/src/main/java/com/tinbobs/chess/server/model/state/GameState.java
+++ server/src/main/java/com/tinbobs/chess/server/model/state/GameState.java
@@ -6,11 +6,35 @@ import com.tinbobs.chess.server.model.board.Position;
 import com.tinbobs.chess.server.model.player.Player;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
-public record GameState(Board board, Player currentTurn) {
+public final class GameState {
+
+    private final Board board;
+    private final Player currentTurn;
+    private final Set<Move> cachedLegalMoves = new HashSet<>();
+
+    public GameState(Board board, Player currentTurn) {
+        this.board = board;
+        this.currentTurn = currentTurn;
+    }
+
+    //getters
+    public Board board() {
+        return this.board;
+    }
+    public Player currentTurn() {
+        return this.currentTurn;
+    }
 
     public Set<Move> getLegalMoves() {
+
+        //only calculate moves once to save computation
+        if (!this.cachedLegalMoves.isEmpty()) {
+            return this.cachedLegalMoves;
+        }
+        
         Set<Move> res = new HashSet<>();
 
         //add all pieces legal moves
@@ -34,5 +58,22 @@ public record GameState(Board board, Player currentTurn) {
 
     public boolean isGameOver() {
         return false;
+    }
+
+
+    //equality
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof GameState s)) {
+            return false;
+        }
+
+        return (
+                (s.board().equals(this.board))
+                && (s.currentTurn().equals(this.currentTurn))
+                );
+    }
+    public int hashCode() {
+        return Objects.hash(this.board, this.currentTurn);
     }
 }
