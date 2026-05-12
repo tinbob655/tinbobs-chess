ca8935ed96b9aa314d481381d311b1718ba82f39
Will Rutland <wrutland07@gmail.com>
2026-05-12 11:13:06 +0100

    player is redundant on move equality

--- server/src/main/java/com/tinbobs/chess/server/model/state/Move.java
+++ server/src/main/java/com/tinbobs/chess/server/model/state/Move.java
@@ -2,5 +2,25 @@ package com.tinbobs.chess.server.model.state;
 
 import com.tinbobs.chess.server.model.board.Position;
 
+import java.util.Objects;
 
-public record Move(Position from, Position to, String playerName) {}
+
+public record Move(Position from, Position to, String playerName) {
+
+    //equality
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Move m)) {
+            return false;
+        }
+
+        return (
+                (m.from().equals(this.from))
+                && (m.to().equals(this.to))
+                );
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.from, this.to);
+    }
+}
