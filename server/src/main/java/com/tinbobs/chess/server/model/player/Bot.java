a3b1b8fa81b7ceeb82abcfb8bc7cc55e2ddc3e5c
Will Rutland <wrutland07@gmail.com>
2026-05-13 13:42:02 +0100

    cut minimax depth slightly

--- server/src/main/java/com/tinbobs/chess/server/model/player/Bot.java
+++ server/src/main/java/com/tinbobs/chess/server/model/player/Bot.java
@@ -10,7 +10,7 @@ import java.util.*;
 
 public final class Bot extends Player {
 
-    private static final int MINIMAX_DEPTH = 6;
+    private static final int MINIMAX_DEPTH = 5;
     private Player opponent;
 
 
